1ac69e7f5eff683f7eabd3aeb7a9cc36907b1f2c
droqen <hi@droqen.com>
2025-02-18 16:01:25 -0500

    moved maze 1 stuff to maze_1 folder

--- maze_1/premain.gd
+++ maze_1/premain.gd
@@ -6,7 +6,7 @@ func _physics_process(delta: float) -> void:
 	position = (get_viewport().size as Vector2 - Vector2(660,660)) * 0.5
 	scale = Vector2(3,3)
 	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://main.tscn")
+		get_tree().change_scene_to_file("res://maze_1/main.tscn")
 	blonk = (blonk+1)%56
 	$press_space_to_play.visible = blonk < 28
 	$Sprite2D.visible = !$press_space_to_play.visible
